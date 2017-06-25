d96107694ee805dafe37449d82f9316d10dd0f3b
DocSohl <docsohl@gmail.com>
2017-06-25 16:59:10 -0700

    Add python-dev to dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 apt-get update
-apt-get install -y libgrib-api-dev
+apt-get install -y libgrib-api-dev python-dev
 
 pip install numpy==1.7.* pyproj==1.9.5.1
 pip install -r requirements.txt
